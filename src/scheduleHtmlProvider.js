09264620c7a526db3458194b6bed77a0514f1a24
grey030 <grey030@foxmail.com>
2021-08-30 20:13:39 +0800

    添加鸣谢名单

--- src/scheduleHtmlProvider.js
+++ src/scheduleHtmlProvider.js
@@ -1,5 +1,10 @@
 function scheduleHtmlProvider(dom = document) {
-    // alert("欢迎使用 :D \n如有问题或错漏，请添加开发者QQ:598383969进行反馈。\n-------\n[最后更新2021-08-30：适应新学期的作息表]\n")
-    const content = dom.getElementsByClassName('el-table__body-wrapper')
-    return content[0].innerHTML;
+  var updatelog =
+    "最后更新于2021-08-30，适配新学期的作息表与教务系统的变动。\n" + "感谢热心同学：嘉、旺仔牛奶的反馈。"
+  alert(
+    "欢迎使用 :D \n如有问题或错漏，请添加开发者QQ:598383969进行反馈。\n------------\n" +
+      updatelog
+  )
+  const content = dom.getElementsByClassName("el-table__body-wrapper")
+  return content[0].innerHTML
 }
