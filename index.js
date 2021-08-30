64867d77b6796bdd53868bc124053a85efcf60fa
grey030 <grey030@foxmail.com>
2021-08-30 19:21:21 +0800

    新增 测试样本2021秋季学期

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 此处必须保留
 var cheerio = require("cheerio")
 
-var htmlFile = "sample3.html"
+var htmlFile = "sample2.html"
 
 console.log("index.js working...")
 
@@ -9,9 +9,9 @@ $(document).ready(function () {
   $("#temp").load("/src/" + htmlFile)
   $("#prompt").append(htmlFile + " 中内容如下：")
 })
+
 // 因此让其等待cheerio加载了之后再延迟执行
 setTimeout(() => {
-  // var html = document.querySelector('#temp').innerHTML
   var html = scheduleHtmlProvider(document)
   $("#temp").empty()
   $("#temp").append(html)
