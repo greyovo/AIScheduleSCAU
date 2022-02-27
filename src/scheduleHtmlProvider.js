3b1d4c78d4a72ac3b5fd6831c66215979022824e
grey030 <grey030@foxmail.com>
2022-02-27 21:18:43 +0800

    update readme

--- src/scheduleHtmlProvider.js
+++ src/scheduleHtmlProvider.js
@@ -1,12 +1,13 @@
 async function scheduleHtmlProvider() {
   var updatelog =
     "🧐 Hi 开发者要毕业了, 有没有热心同学想帮忙维护的, 欢迎 QQ(598383969) 联系我~\n\n" +
-    "📍 最后更新于 2022-02-27, 适配新版小爱课程表版本。感谢大家的反馈！\n\n"+
+    "📍 最后更新于 2022-02-27, 适配新版小爱课程表。感谢大家的反馈！\n\n"+
     "🎉 特别感谢 @液态镁\n";
 
   var info = '\n项目开源地址：github.com/greyovo/AISchedule_SCAU\n'
-  await loadTool('AIScheduleTools')
 
+  await loadTool('AIScheduleTools')
+  
   await AIScheduleAlert({
     titleText: '欢迎使用', // 标题内容，字体比较大，不传默认为提示
     contentText: updatelog + info, // 提示信息，字体稍小，支持使用``达到换行效果，具体使用效果建议真机测试
