8810207ce607a90e7c9b1f6a5afa84305a893c0f
grey030 <grey030@foxmail.com>
2022-02-28 14:35:27 +0800

    更新使用说明

--- index.js
+++ index.js
@@ -25,18 +25,20 @@ setTimeout(async () => {
     // 因为是异步函数，需要 await
     var html = await scheduleHtmlProvider()
     let result = scheduleHtmlParser(html)
-    if(result){
-      $("#output").append(JSON.stringify(result,undefined,2))
+    if(result.length>0){
       $("#status-text").append("成功！").addClass("success")
+      $("#output").append(JSON.stringify(result,undefined,2))
       Prism.highlightAll()
     } else{
-      $("#status-text").append("出现错误，请打开控制台查看").addClass("error")
+      $("#status-text").append("返回的课程长度为 0").addClass("error")
+      $("#output").append(JSON.stringify(result,undefined,2))
+      Prism.highlightAll()
     }
   } catch (error) {
     $("#status-text").append("出现错误，请打开控制台查看").addClass("error")
+    $("#output").append(error)
+    Prism.highlightAll()
   }
-  
-  
   $("#temp").empty()
   $("#temp").append(html)
 }, 500)
